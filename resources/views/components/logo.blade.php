84c179c4da572d128f438d59e225287b02a8dd9c
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2023-08-16 17:13:16 +0200

    Change SVG Logo Markup

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,4 +1,3 @@
-<?xml version="1.0" encoding="UTF-8"?>
 <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 692.63 434.24" class="absolute top-4 right-4 w-28 h-auto z-20">
   <defs>
     <style>
